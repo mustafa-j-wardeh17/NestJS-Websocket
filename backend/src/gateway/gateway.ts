825763100fb51e8180dc46cf884c222313a38122
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-24 11:07:29 +0300

    Creating a Websocket Gateway Server

--- backend/src/gateway/gateway.ts
+++ backend/src/gateway/gateway.ts
@@ -1,13 +1,35 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { OnModuleInit } from "@nestjs/common";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from 'socket.io'
 
-@WebSocketGateway({})
+@WebSocketGateway() // This decorator indicates that this class is a WebSocket gateway. It uses a default port (e.g., 3000 in NestJS).
+//@WebSocketGateway(80) // Uncomment to change the WebSocket gateway to a custom port, such as 80.
 
-export class MyGateway{
+export class MyGateway implements OnModuleInit {
 
+    // To send messages from the gateway back to the client
+    @WebSocketServer()
+    server: Server; // This property represents the WebSocket server instance (Socket.IO server).
 
-    // The gateway is now listening to newMessage
+    
+    // This lifecycle method is triggered when the module is initialized.
+    onModuleInit() {
+        // It sets up an event listener for client connections.
+        this.server.on('connection', (socket) => {
+            console.log(socket.id);
+            console.log('Connected');
+        });
+    }
+
+    // The gateway is now listening for events with the name 'newMessage'.
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body: any) {
-        console.log(body)
+        // This method is triggered when a 'newMessage' event is received.
+
+        this.server.emit('onMessage', {
+            // Emits an 'onMessage' event back to all connected clients with the following data:
+            msg: 'New Message',
+            content: body
+        });
     }
 }
